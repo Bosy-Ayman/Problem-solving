dcff593d6381c14555bee34b254f9c55cd5abf9f
Bosy <112729185+Bosy-Ayman@users.noreply.github.com>
2024-04-27 23:17:11 +0300

    Time: 46 ms (28.73%), Space: 50.8 MB (16.07%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int index = coins.size();
-        vector<vector<int>> dp(index, vector<int>(amount + 1, -1));
-        int result = f(index - 1, amount, coins, dp);
-        return result == 1e9 ? -1 : result; // -1 is not achievable
-    }
-    
-    int f(int index, int target, vector<int>& coins, vector<vector<int>>& dp) {
-        // Base case: from index-1 -> 0
-        if (index == 0) {
-            // target sum is divisible by the first element
-            if (target % coins[0] == 0)
-                return target / coins[0]; 
-            else
-                return 1e9; 
-        }
-        if (dp[index][target] != -1) return dp[index][target];
+        vector<vector<int>> dp(index + 1, vector<int>(amount + 1, 0));
 
-        int notpick = f(index - 1, target, coins, dp);
-        int pick = 1e9;
-        if (target >= coins[index]) {
-            pick = 1 + f(index, target - coins[index], coins, dp); 
+        // Base case: from 0->index-1
+        for (int i = 0; i <= index; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 1; i <= amount; i++) {
+            dp[0][i] = 1e9; 
         }
 
-        return dp[index][target] = min(pick, notpick);
+        
+        for (int ind = 1; ind <= index; ind++) {
+            for (int T = 0; T <= amount; T++) {
+                int notpick = dp[ind - 1][T];
+                int pick = 1e9;
+                if (T >= coins[ind - 1]) {
+                    pick = 1 + dp[ind][T - coins[ind - 1]];
+                }
+                dp[ind][T] = std::min(pick, notpick);
+            }
+        }
+
+        return dp[index][amount] == 1e9 ? -1 : dp[index][amount];
     }
 };
